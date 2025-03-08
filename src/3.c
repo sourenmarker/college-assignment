5ae8ed5ed6defc432a1265a3720992b15325b679
sourenmarker <sourenmarker@gmail.com>
2025-03-08 06:45:02 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,12 @@
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        printf("The value of x is positive.\n");
+    } else if (x < 0) {
+        printf("The value of x is negative.\n");
+    } else {
+        printf("The value of x is zero.\n");
+    }
+    return 0;
+}
