b49bfcfa73a5436f26033d8f32e3c10b301bd0e8
sourenmarker <sourenmarker@gmail.com>
2025-04-11 14:07:33 +0000

    Add 52.c

---
+++ src/52.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int x = 5;
+    int y = 10;
+
+    printf("x + y = %d\n", x + y);
+    printf("x - y = %d\n", x - y);
+    printf("x * y = %d\n", x * y);
+    printf("x / y = %d\n", x / y);
+
+    return 0;
+}
