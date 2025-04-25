bf8a84fd6ef7d3f7ba6d839e7f1193ba529216bf
sourenmarker <sourenmarker@gmail.com>
2025-04-25 14:18:34 +0000

    Add 61.c

---
+++ src/61.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    for(i = 0; i < 10; i++) {
+        if (i % 2 == 0)
+            printf("Even\n");
+        else
+            printf("Odd\n");
+    }
+    return 0;
+}
