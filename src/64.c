2ca9ce50ea2d17a62d5a43b90a4d127060413937
sourenmarker <sourenmarker@gmail.com>
2025-05-17 06:13:13 +0000

    Add 64.c

---
+++ src/64.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    while (i < 10) {
+        printf("%d ", i);
+        i += 2; // Increase the value of 'i' by 2 for each iteration
+    }
+    return 0;
+}
